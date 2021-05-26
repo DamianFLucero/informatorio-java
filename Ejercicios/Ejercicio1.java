d7b6ceed023a9fae89d0432e2abeb64c51601412
Damian Lucero <damianflucero09@gmail.com>
2021-05-26 14:41:50 -0300

    26/05

--- Ejercicios/Ejercicio1.java
+++ Ejercicios/Ejercicio1.java
@@ -1,7 +1,7 @@
 //Realizar un programa que realice la impresión por consola del siguiente mensaje:
 //Hola Informatorio 2021!!
 
-public class HolaInformatorio {
+public class Ejercicio1 {
 
     public static void main(String[] args) {
         System.out.println("Hola Informatorio 2021!!");
---
+++ Ejercicios/Ejercicio3.java
@@ -0,0 +1,43 @@
+import java.util.*;
+//Tendremos un sistema que debe clasificar el resultado de una evaluación. 
+//Como dato de entrada tendremos un entero y como resultado un String de la 
+//clasificación de la siguiente forma:
+
+//Rango       Clasificación
+//93 - 100    Excelente
+//85 - 92     Sobresaliente
+//75 - 84     Distinguido
+//60 - 74     Bueno
+//00 - 59     Desaprobado
+
+
+public class Ejercicio3 {
+    
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();     
+    }
+
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la nota para saber calificación:");
+        int nota = scan.nextInt();
+        scan.close();
+        clearScreen();
+        
+        if (nota > 100){
+            System.out.println("Nota fuera de rango");
+        } else if (nota > 92){
+            System.out.println("Excelente");
+        } else if (nota > 84){
+            System.out.println("Sobresaliente");
+        } else if (nota > 74){
+            System.out.println("Distinguido");
+        } else if (nota > 59){
+            System.out.println("Bueno");
+        } else {
+            System.out.println("Desaprobado");
+        }
+    }
+}
